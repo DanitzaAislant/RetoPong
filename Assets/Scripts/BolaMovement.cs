be7220989358d086ce494de48ec84ba9ab0d69eb
DanitzaAislant <danitza.aislant.2016@upb.edu.co>
2020-08-28 17:54:15 -0500

    Primer reto

---
+++ Assets/Scripts/BolaMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BolaMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _velocity;
+
+    [SerializeField] private float movementForce = 1f;
+
+    private Vector3 direction;
+
+    private void Awake()//Funciona antes del start
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        var direction = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), 0f); //Valores inclusivos, para que la bola salga en una dirección aleatoria.
+        _rigidbody2D.AddForce(direction.normalized * movementForce, ForceMode2D.Impulse); //Mantiene la dirección y el sentido pero no la magnitud.
+    }
+
+    void Update()
+    {
+        _velocity = _rigidbody2D.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var speed = _velocity.magnitude;
+        var newDirection = Vector3.Reflect(_velocity.normalized, other.contacts[0].normal);
+        _rigidbody2D.velocity = newDirection * Mathf.Max(speed, 0f);
+    }
+
+}
---
+++ Assets/Scripts/PadMovement.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// El PadMovement, mueve el jugador a lo largo del eje x.
+/// </summary>
+
+//Lo pone genérico 
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PadMovement : MonoBehaviour
+{   
+    [Tooltip("¡Velocidad en unidades!")]
+    //Velocidad de movimiento, serializable para que Unity la vea.
+    [SerializeField]private float velocity = 5f;
+
+    [Header("Controles para el GamePad:")]
+    [SerializeField] private KeyCode upControl;
+    [SerializeField] private KeyCode downControl;
+
+    private Rigidbody2D _rigidbody2D;
+    private float padLimit = 3.5f;
+   
+   
+    private void awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        //Leer el control de las teclas
+        //Aplicar movimiento
+
+        //Mover hacia arriba
+        if (Input.GetKey(upControl))
+        {
+            transform.Translate(0f, velocity, 0f);
+        }
+
+        //Mover hacia abajo
+        else if (Input.GetKey(downControl))
+        {
+            transform.Translate(0f, -velocity, 0f);
+        }
+
+        transform.position = new UnityEngine.Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -padLimit, padLimit), transform.position.z);
+    }
+}
+
